b69a38493c2946a359872d1151ce96f4886fb686
AVender <vender.aleksandr@gmail.com>
2024-02-15 23:14:34 -0500

    implemented classnames package

--- components/TestTW/TestTW.tsx
+++ components/TestTW/TestTW.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import cn from "classnames";
 
 export const TestTW = () => (
-  <div className="flex text-red-400 border-2 w-24 justify-center">TestTW</div>
+  <div className={cn("flex text-red-400 border-2 w-24 justify-center", {
+    "test": false
+  })}>TestTW</div>
 );
