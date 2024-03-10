ff71697559c221e35e82fea20919fc1e11ca5127
Justin Deng <justin.dengcn@gmail.com>
2024-03-10 19:12:42 -0400

    update comments

--- scripts/deployment/bump-version.cjs
+++ scripts/deployment/bump-version.cjs
@@ -5,16 +5,16 @@ const path = require('path');
 const packageJsonPath = path.join(__dirname, '../../package.json');
 const packageJson = require(packageJsonPath);
 
-let newVersion = process.argv[2]; // Get version from command line argument
+let newVersion = process.argv[2];
 
 if (!newVersion) {
   // If no version provided, bump the minor version
   const versionParts = packageJson.version.split('.');
-  versionParts[1] = parseInt(versionParts[1], 10) + 1; // Increment minor version
+  versionParts[1] = parseInt(versionParts[1], 10) + 1;
   newVersion = versionParts.join('.');
 }
 
-// Update package.json with the new version
+
 packageJson.version = newVersion;
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
 
