ea90ced5f94f6470051f35d8c9bf3b85548ba58b
AVender <vender.aleksandr@gmail.com>
2024-03-19 14:20:41 -0400

    MOD-771: fix typescript

--- components/Table/TableMobile/ItemWrap/index.tsx
+++ components/Table/TableMobile/ItemWrap/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import cn from 'classnames';
+import { MobileItemWrapPropTypes } from '../../types';
 
-export const ItemWrap = ({
+export const ItemWrap: React.FC<MobileItemWrapPropTypes> = ({
   highlightedRows,
   record,
   children,
--- components/index.ts
+++ components/index.ts
@@ -21,3 +21,4 @@ export { Timer } from './Timer';
 export { Picture } from './Picture';
 export { SideBar } from './SideBar';
 export { Table } from './Table';
+export { Input } from './Input';
