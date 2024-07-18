e0a46f7deea5859cc3963a9de708287c42f1b744
Александр Вендер <alex_vender@Aleksandrs-MacBook-Pro.local>
2024-07-18 09:49:37 -0400

    MOD-1131: Added social icons

---
+++ components/Icon/Social/TelegramLogoIcon/index.tsx
@@ -0,0 +1,5 @@
+import { withSvgWrap } from "../../SwgWrap";
+
+export const TelegramLogoIcon = withSvgWrap(
+  "M23.4286 2.28571L0.57143 10.449L7.10204 13.7143L16.898 7.18367L10.3673 15.3469L20.1633 21.8776L23.4286 2.28571Z"
+);
