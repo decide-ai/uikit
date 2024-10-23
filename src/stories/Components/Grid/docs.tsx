80c143a9e8048eab32121b44a08b72a8991f9ca4
Александр Вендер <alex_vender@Aleksandrs-MacBook-Pro.local>
2024-10-23 11:41:57 -0400

    MOD-1491: Fixed grid

--- src/stories/Components/Grid/docs.tsx
+++ src/stories/Components/Grid/docs.tsx
@@ -3,18 +3,21 @@ import React from 'react';
 export const argTypes = {}
 
 export const args = {
+  columns: 12,
+  smColumns: 3,
+  lgColumns: 12,
   children: [
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>1</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>2</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>3</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>4</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>5</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>6</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>7</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>8</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>9</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>10</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>11</div>,
-    <div className='h-24 p-2 flex items-center justify-center font-bold bg-B30'>12</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>1</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>2</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>3</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>4</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>5</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>6</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>7</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>8</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>9</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>10</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>11</div>,
+    <div className='col-span-1 h-24 p-2 flex items-center justify-center font-bold bg-B30'>12</div>,
   ]
 };
